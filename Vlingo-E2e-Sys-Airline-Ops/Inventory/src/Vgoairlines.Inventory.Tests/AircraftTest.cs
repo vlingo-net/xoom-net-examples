3eca365561a6dee506802e7097d6a616dabf8b23
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-03-14 11:47:37 +0100

    Add blueprint of aircraft dispatcher

---
+++ Vlingo-E2e-Sys-Airline-Ops/Inventory/src/Vgoairlines.Inventory.Tests/AircraftTest.cs
@@ -0,0 +1,85 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using System.Collections.Generic;
+using Vlingo.Actors;
+using Vlingo.Common;
+using Vlingo.Lattice.Model.Stateful;
+using Vlingo.Symbio;
+using Vlingo.Symbio.Store.Dispatch;
+using Vlingo.Symbio.Store.State;
+using Vlingo.Symbio.Store.State.InMemory;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Vgoairlines.Inventory.Tests
+{
+    // public class AircraftTest : IDisposable
+    // {
+    //     private static readonly AtomicInteger RaceConditions = new AtomicInteger(0);
+    //     private readonly MockTextDispatcher _dispatcher;
+    //     private readonly Random _idGenerator = new Random();
+    //     private readonly World _world;
+    //     private readonly StateAdapterProvider _stateAdapterProvider;
+    //     
+    //     [Fact]
+    //     public void TestThatStatefulEntityPreservesRestores()
+    //     {
+    //         var entityId = $"{_idGenerator.Next(10_000)}";
+    //         var state = new Entity1State(entityId, "Sally", 23);
+    //         var access = _dispatcher.AfterCompleting(3);
+    //
+    //         var entity1 = _world.ActorFor<IEntity1>(() => new Entity1Actor(RaceConditions, entityId));
+    //         Assert.Equal(state, entity1.DefineWith(state.Name, state.Age).Await());
+    //         Assert.Equal(state, entity1.Current().Await());
+    //
+    //         entity1.ChangeName("Sally Jane");
+    //         var newState = entity1.Current().Await();
+    //         Assert.Equal("Sally Jane", newState.Name);
+    //
+    //         entity1.IncreaseAge();
+    //         newState = entity1.Current().Await();
+    //         Assert.Equal(24, newState.Age);
+    //
+    //         var restoredEntity1 = _world.ActorFor<IEntity1>(() => new Entity1Actor(RaceConditions, entityId));
+    //         var restoredEntity1State = restoredEntity1.Current().Await();
+    //         Assert.NotNull(restoredEntity1State);
+    //
+    //         Assert.Equal(1, access.ReadFrom<int>("dispatchedStateCount"));
+    //         var ids = access.ReadFrom<IEnumerable<string>>("dispatchedIds").ToList();
+    //         Assert.Single(ids);
+    //
+    //         var flatState = access.ReadFrom<string, TextState>("dispatchedState", ids[0]);
+    //         Assert.Equal(new Entity1State(entityId, "Sally Jane", 24), _stateAdapterProvider.FromRaw<Entity1State, TextState>(flatState));
+    //
+    //         restoredEntity1.Current().AndThenConsume(current => {
+    //             Assert.Equal(new Entity1State(entityId, "Sally Jane", 24), current);
+    //         });
+    //     }
+    //     
+    //     public AircraftTest(ITestOutputHelper output)
+    //     {
+    //         var converter = new Converter(output);
+    //         Console.SetOut(converter);
+    //         
+    //         _world = World.StartWithDefaults("stateful-entity");
+    //         _dispatcher = new MockTextDispatcher();
+    //
+    //         _stateAdapterProvider = new StateAdapterProvider(_world);
+    //         _stateAdapterProvider.RegisterAdapter(new Entity1StateAdapter());
+    //         new EntryAdapterProvider(_world);
+    //         var registry = new StatefulTypeRegistry<Entity1State>(_world);
+    //         
+    //         var store = _world.ActorFor<IStateStore<Entity1State>>(() => new InMemoryStateStoreActor<TextState, Entity1State>(new List<IDispatcher<IDispatchable<Entity1State, TextState>>> {_dispatcher}));
+    //         
+    //         registry.Register(new Info<Entity1State>(store, nameof(Entity1State)));
+    //     }
+    //
+    //     public void Dispose() => _world.Terminate();
+    // }
+}
---
+++ Vlingo-E2e-Sys-Airline-Ops/Inventory/src/Vgoairlines.Inventory.Tests/MockTextDispatcher.cs
@@ -0,0 +1,77 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Vgoairlines.Inventory.Model;
+using Vlingo.Actors.TestKit;
+using Vlingo.Common;
+using Vlingo.Symbio;
+using Vlingo.Symbio.Store.Dispatch;
+
+namespace Vgoairlines.Inventory.Tests
+{
+    // public class MockTextDispatcher : IDispatcher<IDispatchable<AircraftState, AircraftState>>
+    // {
+    //     private AccessSafely _access;
+    //
+    //     private IDispatcherControl _control;
+    //     private readonly Dictionary<string, TextState> _dispatched = new Dictionary<string, TextState>();
+    //     private readonly ConcurrentBag<Entity1State> _dispatchedEntries = new ConcurrentBag<Entity1State>();
+    //     private readonly AtomicBoolean _processDispatch = new AtomicBoolean(true);
+    //     
+    //     public MockTextDispatcher() => _access = AfterCompleting(0);
+    //
+    //     public void ControlWith(IDispatcherControl control) => _control = control;
+    //
+    //     public void Dispatch(IDispatchable<Entity1State, TextState> dispatchable)
+    //     {
+    //         if (_processDispatch.Get())
+    //         {
+    //             var dispatchId = dispatchable.Id;
+    //             _access.WriteUsing("dispatched", dispatchId, new Dispatch<TextState, Entity1State>(dispatchable.TypedState<TextState>(), dispatchable.Entries));
+    //         }
+    //     }
+    //     
+    //     public AccessSafely AfterCompleting(int times)
+    //     {
+    //         _access = AccessSafely.AfterCompleting(times)
+    //             .WritingWith<string, Dispatch<TextState, Entity1State>>("dispatched", (id, dispatch) =>
+    //             {
+    //                 _dispatched[id] = dispatch.State;
+    //                 foreach (var entry in dispatch.Entries)
+    //                 {
+    //                     _dispatchedEntries.Add(entry);
+    //                 }
+    //             })
+    //             .ReadingWith("dispatchedIds", () => _dispatched.Keys)
+    //             .ReadingWith<string, TextState>("dispatchedState", id => _dispatched[id])
+    //             .ReadingWith("dispatchedStateCount", () => _dispatched.Count)
+    //
+    //             .WritingWith<bool>("processDispatch", flag => _processDispatch.Set(flag))
+    //             .ReadingWith("processDispatch", () => _processDispatch.Get())
+    //
+    //             .ReadingWith("dispatched", () => _dispatched);
+    //
+    //         return _access;
+    //     }
+    //
+    //     public List<IDispatchable<Entity1State, TextState>> GetDispatched() => _access.ReadFrom<List<IDispatchable<Entity1State, TextState>>>("dispatched");
+    // }
+    //
+    // internal class Dispatch<TState, TEntry>
+    // {
+    //     public List<TEntry> Entries { get; }
+    //     public TState State { get; }
+    //
+    //     public Dispatch(TState state, List<TEntry> entries)
+    //     {
+    //         State = state;
+    //         Entries = entries;
+    //     }
+    // }
+}
